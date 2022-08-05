c090137179fc9e361a192c0a8bd0ad8041d4c449
Tanelo <tanello.mondaca@gmail.com>
2022-08-05 10:11:16 -0400

    router link y sidebar component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 
@@ -9,7 +11,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
    SidebarComponent
