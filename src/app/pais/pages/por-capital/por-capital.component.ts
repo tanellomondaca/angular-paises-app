ab0c6669cb22913aa7b9244e82e618c76ff96cc9
Tanelo <tanello.mondaca@gmail.com>
2022-08-05 10:33:47 -0400

    componente por país

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-por-capital',
-  templateUrl: './por-capital.component.html',
-  styleUrls: ['./por-capital.component.css']
+  templateUrl: './por-capital.component.html'
 })
 export class PorCapitalComponent implements OnInit {
 
